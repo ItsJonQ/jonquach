d332fd8beb6d83138724899d3550049a9b037b0d
Jon Q <hello@jonquach.com>
2020-12-29 03:03:53 -0500

    Tidy fs API. Adjust featured image to use next/image

--- components/post/featured-image.js
+++ components/post/featured-image.js
@@ -1,19 +1,21 @@
 import React from "react";
+import Image from "next/image";
 import { styled } from "@wp-g2/styles";
 
 function PostFeaturedImage({ src }) {
 	return (
 		<FeaturedImageUI>
-			<PlainImage src={src} />
+			<PlainImage src={src} height="1080" width="1920" priority />
 		</FeaturedImageUI>
 	);
 }
 
 const FeaturedImageUI = styled("div")`
 	margin-bottom: 2em;
+	position: relative;
 `;
 
-const PlainImage = styled("img")`
+const PlainImage = styled(Image)`
 	max-width: 100%;
 	height: auto;
 	display: block;
